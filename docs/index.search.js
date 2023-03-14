3ea45a82eb8cf88969372909b91df995de1256fa
Piotr Zawadzki <piotr.zawadzki@pl.ibm.com>
2023-03-14 10:04:23 +0100

    new:

---
+++ docs/index.search.js
@@ -0,0 +1,51 @@
+var relearn_search_index = [
+  {
+    "content": "File to be presented: abcder Related files doc.txt (10 B) plik_tst.txt (12 B) plik.txt (22 B) Related files doc.txt (10 B) plik_tst.txt (12 B) plik.txt (22 B) graph LR; If --\u003e Then Then --\u003e Else %%{init:{\"fontFamily\":\"monospace\", \"sequence\":{\"showSequenceNumbers\":true}}}%% sequenceDiagram Alice-\u003e\u003eJohn: Hello John, how are you? loop Healthcheck John-\u003e\u003eJohn: Fight against hypochondria end Note right of John: Rational thoughts! John--\u003e\u003eAlice: Great! John-\u003e\u003eBob: How about you? Bob--\u003e\u003eJohn: Jolly good! ",
+    "description": "",
+    "tags": null,
+    "title": "Attachement - test",
+    "uri": "/dbateam/databases/database/attachement/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Databases",
+    "uri": "/dbateam/databases/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Db db",
+    "uri": "/dbateam/databases/database/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "DBA Databases",
+    "uri": "/dbateam/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "HUGO",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
